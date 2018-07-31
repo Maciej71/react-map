8599af15b1c5543633312086a38943ad3b2bdf91
giza <giza@transporeon.com>
2018-07-31 16:59:33 +0200

    Add search

--- src/List.js
+++ src/List.js
@@ -1,15 +1,51 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import escapeRegExp from 'escape-string-regexp';
 
 export default class List extends Component {
 	static propTypes = {
 		places: PropTypes.array.isRequired
 	}
+
+	state = {
+    query: ''
+  }
+
+	updateQuery = (query) => {
+	  this.setState({ query: query })
+  }
+
 	render() {
-		return ( 
+		const { places } = this.props
+    const { query } = this.state
+
+	let displayPlaces
+	if (query) {
+		const match = new RegExp(escapeRegExp(query), 'i')
+    displayPlaces = places.filter((place) => match.test(place.name))
+	} else {
+		displayPlaces = places
+	}
+
+		return (
 			<div>
-                <p>blalvlalvlvs</p>
-            </div>
+				<div className="filter">
+					<input
+						className='search-places'
+						type='text'
+						placeholder='Search places'
+						value={query}
+						onChange={(event) => this.updateQuery(event.target.value)}
+					/>
+				</div>
+			  <ul className="places-grid" role="list">
+					{displayPlaces.map((place) => {
+						return(
+							<li className="list-item" key={place.id} tabIndex="1" role="listitem">{place.name}</li>
+						)
+					})}
+			  </ul>
+      </div>
 		);
 	}
 }
